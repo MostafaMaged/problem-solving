c4729674e9a3f80121303184c869088e5d9aaf19
mostafamaged <mostafa.elghmry96@gmail.com>
2022-06-21 16:33:17 +0200

    tournament winner

--- arrays/TournamentWinner.java
+++ arrays/TournamentWinner.java
@@ -11,7 +11,20 @@ public class TournamentWinner {
     private static final int AWAY_TEAM = 1;
     private static final int INITIAL_POINTS_VALUE = 0;
     private static final int WINNING_POINTS = 3;
-    
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<String>> competitions = new ArrayList<ArrayList<String>>();
+        ArrayList<String> competition1 = new ArrayList<String>(Arrays.asList("HTML", "C#"));
+        ArrayList<String> competition2 = new ArrayList<String>(Arrays.asList("C#", "Python"));
+        ArrayList<String> competition3 = new ArrayList<String>(Arrays.asList("Python", "HTML"));
+        competitions.add(competition1);
+        competitions.add(competition2);
+        competitions.add(competition3);
+        ArrayList<Integer> results = new ArrayList<Integer>(Arrays.asList(0, 0, 1));
+        String expected = "Python";
+        var actual = new TournamentWinner().tournamentWinner(competitions, results);
+    }
+
     public String tournamentWinner(ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results) {
 
         String bestTeam = "";
